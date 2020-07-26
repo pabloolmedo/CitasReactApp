ceed4c7dc0010b85102bcfbfb1858d6e21323211
Pablo Olmedo <pablo.olmedo@cba.gob.ar>
2020-07-26 15:39:58 +1000

    Updates

---
+++ precache-manifest.4c823891991ff766b56fa75cf01432ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a41a8c6ae7bd34af8d556d9949c006",
+    "url": "/CitasReactApp/index.html"
+  },
+  {
+    "revision": "36f87d20560e0f70d16c",
+    "url": "/CitasReactApp/static/css/main.f0022edd.chunk.css"
+  },
+  {
+    "revision": "0012f5a03a153668762a",
+    "url": "/CitasReactApp/static/js/2.2aca169f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CitasReactApp/static/js/2.2aca169f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36f87d20560e0f70d16c",
+    "url": "/CitasReactApp/static/js/main.e91093cb.chunk.js"
+  },
+  {
+    "revision": "307a8dc29eb19b8691ed",
+    "url": "/CitasReactApp/static/js/runtime-main.ebe64f40.js"
+  }
+]);
